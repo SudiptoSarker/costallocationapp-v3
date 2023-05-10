9fcf982554c89a0fc95dcb5ef14dfcbcb33afb4d
sarke <sarke@SUDIPTO>
2023-05-10 23:49:47 +0600

    merge conflict

--- CostAllocationApp/Scripts/users_ver02.js
+++ CostAllocationApp/Scripts/users_ver02.js
@@ -32,6 +32,7 @@ $(document).on('click', '#btn_status_change', function () {
 });
 
 $(document).ready(function () {
+
     $.getJSON('/api/utilities/GetOnlyAdmin/')
         .done(function (data) {
             console.log(data);

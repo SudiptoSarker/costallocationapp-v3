425291cd49dc56b8fc7dc48231c4d67a6a8807f0
Adnan Ahsan <adnan.jabait@gmail.com>
2023-10-13 16:33:26 +0600

    Half done: dash UI

---
+++ CostAllocationApp/Controllers/Api/DashController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using CostAllocationApp.BLL;
+
+namespace CostAllocationApp.Controllers.Api
+{
+    public class DashController : ApiController
+    {
+        public DashController()
+        {
+
+        }
+    }
+}
